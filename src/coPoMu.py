8a6a5ec60fc3915d5021ca7516926cb1ac2ea4c4
ZIONKIM <rlswldhs@naver.com>
2021-08-30 13:25:38 +0900

    0830

--- src/coPoMu.py
+++ src/coPoMu.py
@@ -1,11 +1,11 @@
-file_name = "coPoMu.txt"
+file_name = "rosalind_hamm.txt"
 l = []
 cnt = 0
 with open(file_name, "r") as handle:
     for line in handle:
         l.append(line)
     for i in range(len(line)):
-        if l[0][i] == l[1][i]:
+        if l[0][i] == l[1][i]:  # l에 비교할 2개의 string이 저장되어있음.
             continue
         else:
             cnt += 1
